b6c25186d511138dceb6c30b85ce20f7dcf30bca
Robert Livingston <rll@bu.edu>
2022-03-25 12:54:47 -0500

    System Working - Leeroy

---
+++ GildedRoseChallenge/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GildedRoseChallenge
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You Shouldn't Be Seeing This");
+            Console.WriteLine("Check out the read me on how to work with this project.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GildedRoseChallenge_Test/Engine/GildedRose_Should_.cs
@@ -0,0 +1,32 @@
+﻿using GildedRoseChallenge;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace GildedRoseChallenge_Test.Engine
+{
+    public class GildedRose_Should_
+    {
+        [Fact]
+        public void Update_The_Quality_Of_Aged_Brie_To_Increase_With_Time()
+        {
+            var testQuality = 10;
+            var expectedQuality = testQuality + 1;
+            Item testItem = new Item
+            {
+                Name = "Aged Brie",
+                Quality = testQuality,
+                SellIn = 0
+            };
+
+            var items = new List<Item> { testItem };
+
+            var engine = new GildedRose(items);
+            engine.UpdateQuality();
+
+            var actualQuality = items.First().Quality;
+
+            Assert.Equal(expectedQuality + 1, actualQuality);
+        }
+    }
+}
