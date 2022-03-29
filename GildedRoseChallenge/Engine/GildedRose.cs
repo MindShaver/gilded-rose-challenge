7bac0efb7e6835a9e6273bc8cec5e05a3cacc1f7
Robert Livingston <rll@bu.edu>
2022-03-29 14:34:16 -0500

    abstract something

--- GildedRoseChallenge/Engine/GildedRose.cs
+++ GildedRoseChallenge/Engine/GildedRose.cs
@@ -58,36 +58,40 @@ namespace GildedRoseChallenge
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
-                if (Items[i].SellIn < 0)
+                FinalCheck(Items[i]);
+            }
+        }
+
+        public void FinalCheck(Item item)
+        {
+            if (item.SellIn < 0)
+            {
+                if (item.Name != "Aged Brie")
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Quality > 0)
                         {
-                            if (Items[i].Quality > 0)
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
+                                item.Quality = item.Quality - 1;
                             }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
         }
-
     }
 
     public class Item
