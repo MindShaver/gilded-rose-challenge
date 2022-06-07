5c661d67b42fc51f25455fa9fe05b5078d937d20
nash-stokes-int <nash.stokes@integrate.com>
2022-06-07 14:48:36 -0500

    Expanding coverage

--- GildedRoseChallenge_Test/Engine/GildedRose_Should_.cs
+++ GildedRoseChallenge_Test/Engine/GildedRose_Should_.cs
@@ -11,11 +11,53 @@ namespace GildedRoseChallenge_Test.Engine
         [Fact]
         public void Update_The_Quality_Of_Aged_Brie_To_Increase_With_Time()
         {
-            var testQuality = 10;
+            const int testQuality = 10;
             var expectedQuality = testQuality + 1;
             Item testItem = new Item
             {
                 Name = "Aged Brie",
+                Quality = testQuality,
+                SellIn = 0
+            };
+
+            var items = new List<Item> { testItem };
+
+            var engine = new GildedRose(items);
+            engine.UpdateQuality();
+
+            var actualQuality = items.First().Quality;
+
+            Assert.Equal(expectedQuality, actualQuality);
+        }
+        [Fact]
+        public void Update_The_Quality_Of_Backstage_Pass_To_Change_With_Time()
+        {
+            int testQuality = 10;
+            var expectedQuality = testQuality + 2;
+            Item testItem = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                Quality = testQuality,
+                SellIn = 10
+            };
+
+            var items = new List<Item> { testItem };
+
+            var engine = new GildedRose(items);
+            engine.UpdateQuality();
+
+            var actualQuality = items.First().Quality;
+
+            Assert.Equal(expectedQuality, actualQuality);
+        }
+        [Fact]
+        public void Not_Update_The_Quality_Of_Sulfuras_To_Increase_With_Time()
+        {
+            var testQuality = 10;
+            var expectedQuality = 10;
+            Item testItem = new Item
+            {
+                Name = "Sulfuras, Hand of Ragnaros",
                 Quality = testQuality,
                 SellIn = 0
             };
