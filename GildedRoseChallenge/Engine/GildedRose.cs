5909ab04767cfe1b96d6dadb9a700ee60228beee
blakemccracken <blake.mccracken@integrate.com>
2023-03-03 10:26:51 -0800

    refactoring Aged Brie and Backstage pass

--- GildedRoseChallenge/Engine/GildedRose.cs
+++ GildedRoseChallenge/Engine/GildedRose.cs
@@ -13,9 +13,11 @@ namespace GildedRoseChallenge
 
         public void UpdateQuality()
         {
-
             for (var i = 0; i < Items.Count; i++)
             {
+                UpdateAgedBrie(Items[i]);
+                UpdateBackstagePass(Items[i]);
+                
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
@@ -26,37 +28,38 @@ namespace GildedRoseChallenge
                         }
                     }
                 }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
+                // else
+                // {
+                //     if (Items[i].Quality < 50)
+                //     {
+                //          // this will mess up Backstage Pass until we refactor it
+                //          // Items[i].Quality = Items[i].Quality + 1;
+                //
+                //         // if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                //         // {
+                //         //     if (Items[i].SellIn < 11)
+                //         //     {
+                //         //         if (Items[i].Quality < 50)
+                //         //         {
+                //         //             Items[i].Quality = Items[i].Quality + 1;
+                //         //         }
+                //         //     }
+                //         //
+                //         //     if (Items[i].SellIn < 6)
+                //         //     {
+                //         //         if (Items[i].Quality < 50)
+                //         //         {
+                //         //             Items[i].Quality = Items[i].Quality + 1;
+                //         //         }
+                //         //     }
+                //         // }
+                //     }
+                // }
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
+                // if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                // {
+                //     Items[i].SellIn = Items[i].SellIn - 1;
+                // }
 
                 if (Items[i].SellIn < 0)
                 {
@@ -77,17 +80,50 @@ namespace GildedRoseChallenge
                             Items[i].Quality = Items[i].Quality - Items[i].Quality;
                         }
                     }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
+                    // else
+                    // {
+                    //     if (Items[i].Quality < 50)
+                    //     {
+                    //         // aged brie gets updated a second time here
+                    //         Items[i].Quality = Items[i].Quality + 1;
+                    //     }
+                    // }
                 }
             }
         }
 
+        private void UpdateAgedBrie(Item item)
+        {
+            if (item.Name == "Aged Brie" && item.Quality < 50)
+            {
+                item.SellIn -= 1;
+                item.Quality += 1;
+            }
+        }
+        
+        private void UpdateBackstagePass(Item item)
+        {
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert" && item.Quality < 50)
+            {
+                switch (item.SellIn)
+                {
+                    case > 11:
+                        item.Quality += 1;
+                        break;
+                    case < 11 and > 5 :
+                        item.Quality += 2;
+                        break;
+                    case < 6 and > 0 :
+                        item.Quality += 3;
+                        break;
+                    default:
+                        item.Quality = 0;
+                        break;
+                }
+                
+                item.SellIn -= 1;
+            }
+        }
     }
 
     public class Item
--- GildedRoseChallenge_Test/Engine/GildedRose_Should_.cs
+++ GildedRoseChallenge_Test/Engine/GildedRose_Should_.cs
@@ -1,1 +1,141 @@
-﻿
+﻿using System.Collections.Generic;
+using System.Linq;
+using GildedRoseChallenge;
+using Xunit;
+
+public class GildedRose_Should_
+{
+    [Fact]
+    void Update_The_Quality_Of_Finely_Aged_Brie_To_Increase_With_Time()
+    {
+        const int testQuality = 10;
+        const int expectedQuality = testQuality + 1;
+        var testItem = new Item
+        {
+            Name = "Aged Brie",
+            Quality = testQuality,
+            SellIn = -2
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    [Fact]
+    void Update_The_Quality_Of_Backstage_Passes_To_Increase_By_1()
+    {
+        const int testQuality = 10;
+        const int expectedQuality = testQuality + 1;
+        var testItem = new Item
+        {
+            Name = "Backstage passes to a TAFKAL80ETC concert",
+            Quality = testQuality,
+            SellIn = 20
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    [Fact]
+    void Update_The_Quality_Of_Backstage_Passes_To_Increase_By_2()
+    {
+        const int testQuality = 10;
+        const int expectedQuality = testQuality + 2;
+        var testItem = new Item
+        {
+            Name = "Backstage passes to a TAFKAL80ETC concert",
+            Quality = testQuality,
+            SellIn = 10
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    [Fact]
+    void Update_The_Quality_Of_Backstage_Passes_To_Increase_By_3()
+    {
+        const int testQuality = 10;
+        const int expectedQuality = testQuality + 3;
+        var testItem = new Item
+        {
+            Name = "Backstage passes to a TAFKAL80ETC concert",
+            Quality = testQuality,
+            SellIn = 5
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    [Fact]
+    void Update_The_Quality_Of_Backstage_Passes_To_0()
+    {
+        const int testQuality = 10;
+        const int expectedQuality = 0;
+        var testItem = new Item
+        {
+            Name = "Backstage passes to a TAFKAL80ETC concert",
+            Quality = testQuality,
+            SellIn = 0
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    [Fact]
+    void Not_Update_The_Quality_Of_Sulfuras()
+    {
+        const int testQuality = 80;
+        const int expectedQuality = testQuality;
+        var testItem = new Item
+        {
+            Name = "Backstage passes to a TAFKAL80ETC concert",
+            Quality = testQuality,
+            SellIn = 60
+        };
+
+        var items = new List<Item> { testItem };
+
+        var engine = new GildedRose(items);
+        engine.UpdateQuality();
+
+        var actualQuality = items.First().Quality;
+
+        Assert.Equal(expectedQuality, actualQuality);
+    }
+    
+    //TODO: make sure that we are cover "Quality is never below 0"
+}
